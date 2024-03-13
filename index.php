557bd031b37c5476025f3fa1d88ba8d5bfd677c0
insomniaZzz0 <160507585+insomniaZzz0@users.noreply.github.com>
2024-03-13 11:58:39 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>pg teste html</h1>
+    <p>teste 2</p>
+    
+    <?php
+            echo "Olá, eu sou um script PHP!";
+        ?>
+    
+</body>
+</html>
